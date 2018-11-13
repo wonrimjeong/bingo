b4b79129724443c9dca7f501dbbcb4efd8d8328a
wonrimjeong <wonrim0802@gmail.com>
2018-11-14 02:06:18 +0900

    bingo1
    빙고판만들기

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N	5
+#define M	1
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i,j;
+	int s[i][j];
+	
+	srand((unsigned)time(NULL));
+	
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			s[i][j] = 1+rand()%N*N;
+		}
+	}
+	
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			printf("%d", &s[i][j]);
+		}		
+}
+	return 0;
+}
