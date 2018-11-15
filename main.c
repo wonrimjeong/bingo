4194e0c2eeeb2aca81adb9f052e97efadc50901d
wonrimjeong <wonrim0802@gmail.com>
2018-11-15 18:06:21 +0900

    03
    aaa

--- main.c
+++ main.c
@@ -11,20 +11,25 @@ int main(int argc, char *argv[]) {
 	
 	int i,j;
 	int s[i][j];
+	int max = N*N;
 	
 	srand((unsigned)time(NULL));
 	
-	for(i=0;i<N;i++){
-		for(j=0;j<N;j++){
-			s[i][j] = 1+rand()%N*N;
+	for(i=0;i<N;i++)
+	{
+		for(j=0;j<N;j++)
+		{
+			s[i][j] = 1+rand()%max;
 		}
 	}
-	
-	for(i=0;i<N;i++){
-		for(j=0;j<N;j++){
-			printf("%d", &s[i][j]);
-			printf("\n");
-		}		
-}
+
+	for(i=0;i<N;i++)
+	{
+		for(j=0;j<N;j++)
+		{
+			printf(" %d ", s[i][j]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
