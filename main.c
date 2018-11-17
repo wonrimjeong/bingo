8481f005b6f7722fc5c510e08c643bc3554596f9
wonrimjeong <wonrim0802@gmail.com>
2018-11-18 00:30:59 +0900

    08
    aa

--- main.c
+++ main.c
@@ -7,30 +7,49 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void initiate_bingo(int (*p)[N], int max);
+void initiate_bingo(int(*p)[N], int N*N);
+void print_bingo(int(*p)[N]);
 int main(int argc, char *argv[])
 {
 
    int num1, num2, i, j;
    int s[N][N], c[N][N];
-   int max = N * N;
 
    srand((unsigned)time(NULL));
 
-   initiate_bingo(s, max);
-   initiate_bingo(c, max);
+   initiate_bingo(s, N*N);
+   initiate_bingo(c, N*N);
+   print_bingo(s);
+   printf("\n");
+   print_bingo(c);
+
    
+
+
    return 0;
 }
-void initiate_bingo(int (*p)[N], int max)
+void initiate_bingo(int(*p)[N], int N*N)
 {
-   int i,j;
+   int i, j;
 
    for (i = 0; i < N; i++)
    {
       for (j = 0; j < N; j++)
       {
-         p[i][j] = 1 + rand() % max;
+         p[i][j] = 1 + rand() % (N*N);
       }
+   }
+}
+void print_bingo(int(*p)[N])
+{
+   int i, j;
+
+   for (i = 0; i < N; i++)
+   {
+      for (j = 0; j < N; j++)
+      {
+         printf(" %2d ", p[i][j]);
+      }
+      printf("\n");
    }
 }
