cdd16a0c0c1410b31849694d9771961354d03cc7
wonrimjeong <wonrim0802@gmail.com>
2018-11-18 13:25:53 +0900

    10
    aa

--- main.c
+++ main.c
@@ -9,13 +9,13 @@
 void initiate_bingo(int(*p)[N]);
 void print_bingo(int(*p)[N]);
 int get_number_byMe();
-int get_number_byCom();
-void process_bingo(int num);
+int get_number_byCom(int(*p)[N]);
+void process_bingo(int(*p)[N], int num);
 int count_bingo(int(*p)[N]);
 int main(int argc, char *argv[])
 {
 
-   int num1, num2, i, j;
+   int NUM, i, j;
    int s[N][N], c[N][N];
 
    srand((unsigned)time(NULL));
@@ -25,14 +25,18 @@ int main(int argc, char *argv[])
    print_bingo(s);
    printf("\n");
    print_bingo(c);
+   printf("\n");
 
    while (1)
    {
-      num1 = get_number_byMe();
-      num2 = get_number_byCom();
-      process_bingo(num1);
-      process_bingo(num2);
+      NUM = get_number_byMe();
+      process_bingo(s, NUM);
+      process_bingo(c, NUM);
+      print_bingo(s);
+      printf("\n");
+      print_bingo(c);
 
+      
 
    }
 
@@ -48,27 +52,27 @@ void initiate_bingo(int(*p)[N])
    {
       for (j = 0; j < N; j++)
       {
-         ran = 1 + rand() % (N*N);
          while (1)
          {
+            ran = 1 + rand() % (N*N);
             for (k = 0; k < cnt; k++)
             {
                if (tmp[k] == ran)
-               {
-                  ran = 1 + rand() % (N*N);
                   break;
-               }
                else
                   check++;
             }
             if (check == cnt)
+            {
+               check = 0;
                break;
-            check = 0;
+            }
+            else
+               check = 0;
          }
          p[i][j] = ran;
          tmp[cnt]= ran;
          cnt++;
-         
       }
    }
 }
@@ -79,9 +83,7 @@ void print_bingo(int(*p)[N])
    for (i = 0; i < N; i++)
    {
       for (j = 0; j < N; j++)
-      {
          printf("%3d", p[i][j]);
-      }
       printf("\n");
    }
 }
@@ -92,11 +94,45 @@ int get_number_byMe()
    scanf("%d", &num);
    return num;
 }
-int get_number_byCom()
+int get_number_byCom(int(*p)[N])
 {
-   return rand() % (N*N);
+   int i, j, ran, check=0;
+
+   while (1)
+   {
+      ran = 1 + rand() % (N*N);
+      for (i = 0; i < N; i++)
+      {
+         for (j = 0; j < N; j++)
+         {
+            if (p[i][j] == ran)
+               break;
+            else
+               check++;
+         }
+         if (p[i][j] == ran)
+            break;
+      }
+      if (p[i][j] == ran)
+         return ran;
+      check = 0;
+   }
+   
 }
-void process_bingo(int num)
+void process_bingo(int(*p)[N], int num)
 {
+   int i, j;
+
+   for (i = 0; i < N; i++)
+   {
+      for (j = 0; j < N; j++)
+      {
+         if (p[i][j] == num)
+            break;
+      }
+      if (p[i][j] == num)
+         break;
+   }
+   p[i][j] = -1;
 
 }
